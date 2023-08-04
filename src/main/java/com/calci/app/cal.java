f478080eceac9e1769615d64aab08c7ea5ddb380
Atif hasan <Atif9258753479@gmail.com>
2023-08-04 12:40:38 +0530

    mvn_files

---
+++ src/main/java/com/calci/app/cal.java
@@ -0,0 +1,7 @@
+package com.calci.app;
+public class cal{
+    public static int Add(int a,int b){
+
+        return a+b;
+    }
+}
---
+++ src/test/java/com/calci/app/testCalci.java
@@ -0,0 +1,14 @@
+package com.calci.app;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class testCalci
+
+{
+    @Test
+    void testAdd()
+    {
+        assertEquals(10,cal.Add(5, 5));
+
+    }
+}
